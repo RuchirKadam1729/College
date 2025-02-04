bb4e0c166aa6006e45a86ac581d00585452dbdbd
justmessinaround <RuchirKadam1729@gmail.com>
2025-02-04 17:34:13 +0530

    f

---
+++ Sem4/CCN/Exp2/WebsiteEvaluationScript.py
@@ -0,0 +1,70 @@
+import json
+import pandas as pd
+from statistics import mean
+
+'''
+5. What is the average load time of the webpage without any network throttling?
+6. After applying the "Slow 3G" network profile, how does the page load time change compared to the normal network?
+7. Which resources (e.g., images, scripts, CSS files) take the longest to load when the page is throttled with "Regular 3G"?
+8. Identify any 404 or other errors in the network log when loading the page with "Slow 3G"
+applied. What might cause these errors?
+9. Does the page load faster or slower with "Fast 3G" compared to "Slow 3G"? What are
+the reasons for the differences?
+10. What is the "Time to First Byte (TTFB)" for the webpage under different throttling
+profiles? How does network throttling affect TTFB?
+11. How does the "DOMContentLoaded" time change when testing under different throttling
+profiles?
+12. What is the impact of throttling on the "Load" event timing (when the page is fully
+loaded)?
+13. Is there any noticeable delay in rendering when the page is loaded under "Slow 3G"
+conditions? If so, what resources are responsible for the delay?
+14. Are there any large resources (e.g., images, scripts) that cause the page to load slowly
+under network throttling? Identify them.
+'''
+
+def pages(harFile):
+    har = json.load(open(harFile))
+    return har['log']['pages']
+
+def entries(harFile):
+    har = json.load(open(harFile))
+    return har['log']['entries']
+
+
+def avgLoadTime(harFile):
+    return mean([page['pageTimings']['onLoad'] for page in pages(harFile)])
+
+def loadTimePerMimeType(harFile):
+    listOfDicts = [{entry['response']['content']['mimeType']:entry['timings'].get('wait',0)} for entry in entries(harFile)]
+    return pd.DataFrame(listOfDicts).aggregate('mean')
+
+def check404(harfile):
+    return sum([entry['response']['status']==404 for entry in entries(harfile)])
+
+def avgTTFB(harFile):
+    return mean([entry['timings'].get('wait',0) for entry in entries(harFile)])
+0, 30.533333333333335, 1913, 73, text/html; charset=utf-8                      19510.0
+
+def avgDOMLoadTime(harFile):
+    return mean([page['pageTimings']['onContentLoad'] for page in pages(harFile)])
+
+def avgRenderTime(harFile) : 
+    return avgLoadTime(harFile) - avgDOMLoadTime(harFile)
+
+def sizePerMimeType(harFile):
+    listOfDicts = [\
+        {entry['response']['content']['mimeType']: \
+            entry['response']['content']['size']} \
+        for entry in entries(harFile)]
+    
+    return (
+    pd.DataFrame(listOfDicts)               
+    .replace(0, pd.NA).aggregate('mean')
+    )
+
+
+funcs = [avgLoadTime,loadTimePerMimeType,check404,avgTTFB,avgDOMLoadTime,avgRenderTime,sizePerMimeType]
+
+processed = [func('har/desmos.har') for func in funcs]
+
+print(processed)
---
+++ Sem4/CCN/Exp2/instructions.md
@@ -0,0 +1,18 @@
+5. What is the average load time of the webpage without any network throttling?
+6. After applying the "Slow 3G" network profile, how does the page load time change compared to the normal network?
+7. Which resources (e.g., images, scripts, CSS files) take the longest to load when the
+page is throttled with "Regular 3G"?
+8. Identify any 404 or other errors in the network log when loading the page with "Slow 3G"
+applied. What might cause these errors?
+9. Does the page load faster or slower with "Fast 3G" compared to "Slow 3G"? What are
+the reasons for the differences?
+10. What is the "Time to First Byte (TTFB)" for the webpage under different throttling
+profiles? How does network throttling affect TTFB?
+11. How does the "DOMContentLoaded" time change when testing under different throttling
+profiles?
+12. What is the impact of throttling on the "Load" event timing (when the page is fully
+loaded)?
+13. Is there any noticeable delay in rendering when the page is loaded under "Slow 3G"
+conditions? If so, what resources are responsible for the delay?
+14. Are there any large resources (e.g., images, scripts) that cause the page to load slowly
+under network throttling? Identify them.
