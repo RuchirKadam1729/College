4f9df71fbc7433fdfb4b00aa0bc123cfa260927b
RuchirKadam1729 <RuchirKadam1729@gmail.com>
2025-02-07 01:19:08 +0530

    g

--- Sem4/DAA/ConvexHull/convex.c
+++ Sem4/DAA/ConvexHull/convex.c
@@ -2,51 +2,51 @@
 #include<math.h>
 #define LEN(x,type) sizeof(x)/sizeof(type)
 #include<stdlib.h>
+#include<stdbool.h>
+#include<time.h>
 
 struct Point {
     double x;
     double y;
 };
 
-void map_points(double (*func)(struct Point), int n, struct Point arr_in[n], double arr_out[n])
+void print_point(struct Point p);
+void print_point_arr(struct Point p[], int n);
+void print_arr(int n, double arr[n]);
+
+
+struct Point p0 = {0,0};
+
+struct Point sub(struct Point p1, struct Point p2)
 {
-    for (int i = 0; i < n; i++)
-    {
-        arr_out[i] = func(arr_in[i]);
-    }
+    return (struct Point){.x = p1.x-p2.x,.y = p1.y-p2.y};
 }
 
-void print_point(struct Point p)
+int turn(struct Point p1, struct Point p2, struct Point p3)
 {
-    printf("(%.2lf, %.2lf)",p.x,p.y);
-}
-void print_point_arr(struct Point p[], int n) 
-{
-    printf("[");
-    for (int i = 0; i < n; i++) {
-        print_point(p[i]);
-        printf(", ");
+    struct Point 
+    dp1 = sub(p2, p1),
+    dp2 = sub(p3, p2);
+
+    int det = (dp1.x * dp2.y) - (dp1.y * dp2.x);
+
+    if (det > 0) {
+        return 1;
     }
-    printf("\b\b]");
-}
-void print_arr(double arr[], int n)
-{
-    printf("[");
-    for (size_t i = 0; i < n; i++) {
-        printf("%lf, ", arr[i]);
+    else if (det == 0) {
+        return 0;
     }
-    printf("\b\b]");
-}
-double atan2_point(struct Point p)
-{
-    return atan2(p.y, p.x);
+    else {
+        return -1;
+    }
 }
 
 int compare_by_angle(const void *ptrp1, const void *ptrp2)
 {
     struct Point p1 = *(struct Point *)ptrp1;
     struct Point p2 = *(struct Point *)ptrp2;
-    return (atan2_point(p1) - atan2_point(p2) > 0);
+    
+    return turn(p0, p1, p2);
 }
 
 double sort_points_by_angle(struct Point points[], int n)
@@ -80,14 +80,15 @@ void print_stack(struct Stack s)
         print_point(s.arr[s.top - i]);
         printf(" |\n");
     }
-    //printf("+------+\n");
 }
 
-struct Point sub(struct Point p1, struct Point p2)
+
+bool right_turn(struct Point p1, struct Point p2, struct Point p3)
 {
-    return (struct Point){.x = p1.x-p2.x,.y = p1.y-p2.y};
+    return turn(p1, p2, p3) < 0;
 }
-int main(int anglec, char const *anglev[])
+
+int main()
 {
     struct Point points[10];
     int n = LEN(points, struct Point);
@@ -99,31 +100,52 @@ int main(int anglec, char const *anglev[])
 
     
     double out[n];
-    print_point_arr(points, n);
-    printf("\n");
-    map_points(atan2_point, n, points, out);
-    print_arr(out, n);
-    printf("\n");
+
     sort_points_by_angle(points, n);
     print_point_arr(points, n);
     printf("\n");
+
     struct Stack s = {.top = -1};
     
     push(&s, points[0]);
     push(&s, points[1]);
     push(&s, points[2]);
-    
+
     for (size_t i = 3; i < n; i++)
     {
-        double current_turn_angle = atan2_point(sub(points[i], s.arr[s.top]));
-        double previous_turn_angle = atan2_point(sub(s.arr[s.top], s.arr[s.top - 1]));
-        
-        if (current_turn_angle < previous_turn_angle)
+        while (right_turn(s.arr[s.top], s.arr[s.top-1], s.arr[s.top-2]))
+        {
+            struct Point last = pop(&s);
             pop(&s);
-            
+            push(&s, last);
+        }
         push(&s, points[i]);
     }
     
+
     print_point_arr(s.arr,s.top+1);
+    printf("\n");
     return 0;
+}
+
+void print_point(struct Point p)
+{
+    printf("(%.2lf, %.2lf)",p.x,p.y);
+}
+void print_point_arr(struct Point p[], int n) 
+{
+    printf("[");
+    for (int i = 0; i < n; i++) {
+        print_point(p[i]);
+        printf(", ");
+    }
+    printf("\b\b]");
+}
+void print_arr(int n, double arr[n])
+{
+    printf("[");
+    for (size_t i = 0; i < n; i++) {
+        printf("%lf, ", arr[i]);
+    }
+    printf("\b\b]");
 }
